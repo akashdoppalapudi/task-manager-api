7858050d9ddf5f78ca894ed695b6ea0a61a51d89
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2021-07-20 22:36:17 +0530

    setup basic routes

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send("Hello World");
+/* Route Handlers */
+
+/* List Routes */
+
+/**
+ * GET /lists
+ * Purpose: Get all lists
+ */
+
+app.get('/lists', (req, res) => {
+	// return an array of all the lists in the database
+});
+
+/**
+ * POST /lists
+ * Purpose: Create a list
+ */
+
+app.post('/lists', (req, res) => {
+	// create a list and return the document back to user (including the ID)
+	// Th list information (fields) will be passed in via the JSON request body
+});
+
+/**
+ * PATCH /lists/:id
+ * Purpose: Update a specified list
+ */
+
+app.patch('/lists/:id', (req, res) => {
+	// Update the specified list (list document with id specified in url) with the new values specified in JSON request body
+});
+
+/**
+ * DELETE /lists/:id
+ * Purpose: Delete a specified list
+ */
+
+app.delete('/lists/:id', (req, res) => {
+	// Delete the specified list (list document with id specified in url)
 });
 
 app.listen(3000, () => {
-	console.log("Server Running on http://localhost:3000/");
+	console.log('Server Running on http://localhost:3000/');
 });
---
+++ db/mongoose.js
@@ -0,0 +1,18 @@
+// Handle the connection to the MongoDB Database
+import mongoose from 'mongoose';
+
+mongoose.Promise = global.Promise;
+mongoose
+	.connect(
+		'mongodb+srv://akashdoppalapudi:Akash@1729@cluster0.v7l6s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
+	.then(() => {
+		console.log('Successfully connected to MongoDB :)');
+	})
+	.catch((error) => {
+		console.log('Error While connecting to MongoDB');
+		console.log(error);
+	});
+
+module.exports = { mongoose };
